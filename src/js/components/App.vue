328f5e64d405c437758b43e230935ce74ac35f0b
Jase <jase@star29.com>
2017-09-10 05:25:44 -0600

    Confirm note delete, todo item states, layout structure

--- src/js/components/App.vue
+++ src/js/components/App.vue
@@ -1,10 +1,20 @@
 <template>
   <body class="notes">
+    <svg style="display: none">
+      <symbol viewBox="0 0 34 34" id="x">
+          <path d="M9.45752767,10.4003367 L10.4003367,9.45752767 C10.790861,9.06700338 11.424026,9.06700338 11.8145503,9.45752767 L17,14.6429774 L22.1854497,9.45752767 C22.575974,9.06700338 23.209139,9.06700338 23.5996633,9.45752767 L24.5424723,10.4003367 C24.9329966,10.790861 24.9329966,11.424026 24.5424723,11.8145503 L19.3570226,17 L24.5424723,22.1854497 C24.9329966,22.575974 24.9329966,23.209139 24.5424723,23.5996633 L23.5996633,24.5424723 C23.209139,24.9329966 22.575974,24.9329966 22.1854497,24.5424723 L17,19.3570226 L11.8145503,24.5424723 C11.424026,24.9329966 10.790861,24.9329966 10.4003367,24.5424723 L9.45752767,23.5996633 C9.06700338,23.209139 9.06700338,22.575974 9.45752767,22.1854497 L14.6429774,17 L9.45752767,11.8145503 C9.06700338,11.424026 9.06700338,10.790861 9.45752767,10.4003367 Z" id="+"></path>
+      </symbol>
+    </svg>
     <aside class="sidebar">
       <transition-group name="flip-list" tag="ul">
-        <li v-for="note in notes" :key="note.id" :class="{ active: note === selected }">
-          <span @click="selectNote(note)">{{ note.title }}</span>
-          <span @click="removeNote(note)" v-if="note === selected"><i class="fa fa-times-circle fa-lg"></i></span>
+        <li v-for="note in notes" :key="note.id" :class="{ active: note === selected }" @click="selectNote(note)">
+          <h4 class="note-title">
+            <span>{{ note.title }}</span>
+            <span class="x" @click.stop="tryDelete(note)" v-if="note === selected">
+              <svg><use xlink:href="#x" /></svg>
+            </span>
+          </h4>
+          <div class="note-preview">{{ note.body }}</div>
         </li>
       </transition-group>
       <footer>
@@ -20,6 +30,13 @@
         <editor v-if="selected" v-model="selected.body" :key="selected.id" placeholder="Notes..."></editor>
       </transition>
     </main>
+    <dialog id="👻">
+      <header>Delete Note?</header>
+      <footer>
+        <button @click="cancelDelete()">No</button>
+        <button @click="confirmDelete(selected)">Yes</button>
+      </footer>
+    </dialog>
   </body>
 </template>
 
@@ -29,17 +46,21 @@ import moment from 'moment'
 
 const { chrome } = window
 const SKEY = "SCRIPTO"
+const DIALOG_ID = "👻"
 
 const component = {
   data: () => {
     return {
       notes: [],
       selected: undefined,
-      toolboxIsOpen: false
+      toolboxIsOpen: false,
+      dialog: null,
+      isNewNote: false,
     }
   },
   mounted: function() {
     const vm = this
+    this.dialog = document.getElementById(DIALOG_ID)
     loadNotes(this)
     vm.$watch(function() {
       if (!this.selected) return undefined;
@@ -63,6 +84,10 @@ const component = {
     toggleTools,
     createNote,
     removeNote,
+    tryDelete,
+    cancelDelete,
+    confirmDelete,
+    dismissDialog,
     selectNote,
     clearAllNotes,
   },
@@ -122,6 +147,27 @@ function loadNotes(vm) {
       vm.selectNote(list[0])
     }
   })
+}
+
+function tryDelete() {
+  this.dialog.showModal()
+}
+function cancelDelete() {
+  this.dismissDialog()
+}
+function dismissDialog() {
+  const DIALOG = this.dialog
+  const CALLBACK = function() {
+    DIALOG.classList.remove('hide')
+    DIALOG.close()
+    DIALOG.removeEventListener('animationend', CALLBACK, false)
+  }
+  DIALOG.classList.add('hide')
+  DIALOG.addEventListener('animationend', CALLBACK, false)
+}
+function confirmDelete(note) {
+  this.removeNote(note)
+  this.dismissDialog()
 }
 </script>
 
--- src/js/components/Editor.vue
+++ src/js/components/Editor.vue
@@ -17,34 +17,45 @@
 
   function mounted() {
     const md = initEditor(this.$el.childNodes[0], this.value)
-
-    // Focusing into and setting cursor at the end (100 is long enought)
-    // md.codemirror.focus()
-    md.codemirror.setCursor(100)
-
-    // On change communicate to v-model
     md.codemirror.on('change', () => {
       const val = this.md.value()
       this.$emit('input', val)
     })
-
+    if(this.value) {
+      md.togglePreview()
+    }
     this.md = md
   }
 
   function initEditor(el, val) {
+    class MarkedCustomRenderer extends marked.Renderer {
+      listitem(text) {
+        if (/^\s*\[[dbsx\/\! ]\]\s*/.test(text)) {
+          text = text
+          .replace(/^\s*\[ \]\s*/, '<i class="todo-undone fa fa-circle-o fa-fw"></i> ')
+          .replace(/^\s*\[(x|d)\]\s*/, '<i class="todo-done fa fa-check-circle fa-fw"></i> ')
+          .replace(/^\s*\[(\/|s)\]\s*/, '<i class="todo-started fa fa-play-circle fa-fw"></i> ')
+          .replace(/^\s*\[(!|b|#)\]\s*/, '<i class="todo-blocked fa fa-exclamation-circle fa-fw"></i> ')
+          return '<li class="todo-item" style="list-style: none">' + text + '</li>'
+        } else {
+          return '<li>' + text + '</li>'
+        }
+      }
+    }
+
+    var renderer = new MarkedCustomRenderer()
+
     return new SimpleMDE({
       element: el,
       initialValue: val,
       spellChecker: true,
       placeholder: "Type here...",
-      // toolbar: true,
       toolbarTips: false,
-      status: ["autosave", "lines", "words", "cursor"],
       autoDownloadFontAwesome: true,
       forceSync: true,
       previewRender: function(plainText, preview) { // Async method
         setTimeout(function(){
-          preview.innerHTML = marked(plainText);
+          preview.innerHTML = marked(plainText, {renderer});
         }, 250);
         return "Loading...";
       },
